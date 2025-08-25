5eed2038b2bf9ae04f1a6552189a6cc7a75fd821
eilts-EA <mitchell.eilts@eiltsaerospace.com>
2025-08-24 21:08:09 -0400

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,17 +1,13 @@
 
 from contextlib import contextmanager
-from sqlmodel import SQLModel, create_engine, Session
-import os
+from sqlmodel import SQLModel, Session, create_engine
 
-DB_URL = os.getenv("DATA_GUI_DB_URL", "sqlite:///data.db")
-connect_args = {"check_same_thread": False} if DB_URL.startswith("sqlite") else {}
-
-engine = create_engine(DB_URL, echo=False, pool_pre_ping=True, connect_args=connect_args)
+DATABASE_URL = "sqlite:///database.db"  # or your connection string
+engine = create_engine(DATABASE_URL, echo=True)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-@contextmanager
 def get_session():
     with Session(engine) as session:
         yield session
